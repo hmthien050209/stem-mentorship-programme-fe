f2633af0e9640fbc8ab262f28eacb6008b66d1ff
Longdam2207 <damxuanlong865@gmail.com>
2025-04-28 22:57:04 +0700

    centered and spacing the bar

--- components/BaseNavBar.vue
+++ components/BaseNavBar.vue
@@ -1,22 +1,21 @@
 <template>
   <nav
     id="baseNavBar"
-    class="fixed top-0 left-0 z-10 flex h-24 w-screen items-center justify-center motion-safe:transition-all motion-safe:duration-150 motion-safe:ease-out"
+    class="fixed top-0 left-0 z-10 flex h-16 w-screen items-center justify-center motion-safe:transition-all motion-safe:duration-150 motion-safe:ease-out bg-black/70 text-white"
   >
-    <NuxtLink to="/">Dự án</NuxtLink>
-    <NuxtLink to="/explore">Khám phá</NuxtLink>
-    <NuxtLink to="/about">Về chúng tôi</NuxtLink>
+    <NuxtLink to="/" class="px-6">Dự án</NuxtLink>
+    <NuxtLink to="/explore" class="px-6">Khám phá</NuxtLink>
+    <NuxtLink to="/about" class="px-6">Về chúng tôi</NuxtLink>
   </nav>
 </template>
 
 <style scoped lang="css">
-  @reference "~/assets/css/main.css";
+@reference "~/assets/css/main.css";
 
-  nav {
-    @apply bg-transparent text-white;
-  }
+nav {
+}
 
-  a {
-    @apply mx-1.5;
-  }
+a {
+  @apply mx-4; /* margin-left + margin-right = 1rem */
+}
 </style>
