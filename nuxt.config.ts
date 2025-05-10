2ae59aef859520ee5e0c6aaf5d6b68224b3fb9cf
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-05-10 21:29:07 +0700

    feat: added route rules to optimize page loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,12 @@ export default defineNuxtConfig({
     dirs: ['types'],
   },
 
+  routeRules: {
+    '/': {
+      static: true,
+    },
+  },
+
   sanity: {
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: process.env.SANITY_DATASET,
